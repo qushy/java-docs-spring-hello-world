6649e638898f22b307c7759083773ba9613834aa
qushy <qushaoyan1010@sina.com>
2023-04-16 20:28:20 +0800

    add new api

---
+++ src/main/java/com/example/demo/Hello01.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello01 {
+    
+    @RequestMapping("/world/china/fay")
+	String sayHello() {
+		return "Hello china!";
+	}
+}
